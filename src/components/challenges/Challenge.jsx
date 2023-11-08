a681dc95db3989002f037b9c5219ca0fcf1eedfd
hrshkshri <harshkeshari100@gmail.com>
2023-11-08 06:01:02 +0530

    updated challenge

--- src/components/challenges/Challenge.jsx
+++ src/components/challenges/Challenge.jsx
@@ -1,5 +1,3 @@
-"use client"
-import React from "react";
 import { AiOutlineCalendar } from "react-icons/ai";
 import { FaHackerrank } from "react-icons/fa";
 import { MdGroups2 } from "react-icons/md";
@@ -22,16 +20,18 @@ const Challenge = ({ challenge }) => {
         )}
         <h2 className="text-lg font-semibold">{challenge.name}</h2>
         <p>{challenge.description}</p>
-        <div className="flex items-center space-x-2 mt-2">
-          <MdGroups2 className="text-3xl text-gray-400" />
-          <span className="text-gray-400">{challenge.totalParticipant}</span>
-          <AiOutlineCalendar className="text-3xl text-gray-400" />
-          <span className="text-gray-400">{formattedDate}</span>
+        <div className="flex justify-between">
+          <div className="flex items-center space-x-2 mt-2">
+            <MdGroups2 className="text-3xl text-gray-400" />
+            <span className="text-gray-400">{challenge.totalParticipant}</span>
+            <AiOutlineCalendar className="text-3xl text-gray-400" />
+            <span className="text-gray-400">{formattedDate}</span>
+          </div>
+          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline rounded">
+            Enroll
+          </button>
         </div>
       </div>
-      <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline rounded">
-        Enroll
-      </button>
     </div>
   );
 };
