374ecc3309d49fdc65d108e2b92bd01dd62a4c96
hrshkshri <harshkeshari100@gmail.com>
2023-11-11 19:39:59 +0530

    checkpoint

--- src/App.jsx
+++ src/App.jsx
@@ -72,6 +72,7 @@ function App() {
             </>
           ) : (
             <>
+              <Route path='/' element={<Navigate to='/login' />} />
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
               <Route path="*" element={<NotFound />} />
