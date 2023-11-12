928823786c644a7439787991021e620d1a1c881f
hrshkshri <harshkeshari100@gmail.com>
2023-11-12 18:27:16 +0530

    added leaderboard

--- src/routes/Leaderboard.jsx
+++ src/routes/Leaderboard.jsx
@@ -1,9 +1,35 @@
+import { SiCodechef } from "react-icons/si";
+import { FaHackerrank } from "react-icons/fa";
+import { AiOutlineArrowRight } from "react-icons/ai";
+import { Link } from "react-router-dom";
+
 const Leaderboard = () => {
+
     return (
-        <div className="border border-zinc-700 rounded-xl p-5 bg-zinc-900/10">
-            <p className="text-lg font-semibold text-gray-400">
-                Yet to start a contest...
-            </p>
+        <div className="flex flex-col justify-around space-y-5">
+            <div className="flex items-center justify-between border border-zinc-700 rounded-xl p-5 bg-zinc-900/10">
+                <div className="flex items-center">
+                    <SiCodechef className="text-7xl text-codechef" />
+                    <h2 className="text-lg font-semibold text-gray-400 ml-2">CodeChef Leaderboard</h2>
+                </div>
+                <Link
+                    to={'/leaderboard/codechef'}
+                    className="flex justify-end items-center bg-teal-800 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded"
+                >
+                    View Results
+                    <AiOutlineArrowRight className="ml-2" />
+                </Link>
+            </div>
+            <div className="flex items-center justify-between border border-zinc-700 rounded-xl p-5 bg-zinc-900/10">
+                <div className="flex items-center">
+                    <FaHackerrank className="text-7xl text-hackerrank" />
+                    <h2 className="text-lg font-semibold text-gray-400 ml-2">HackerRank Leaderboard</h2>
+                </div>
+                <button className="flex justify-end items-center bg-teal-800 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded">
+                    View Results
+                    <AiOutlineArrowRight className="ml-2" />
+                </button>
+            </div>
         </div>
     )
 }
